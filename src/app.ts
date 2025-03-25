7d1b5f97ebbc5e667d0b2d1e1e7ede2904eee13a
mahi25soni <msdraj02@protonmail.com>
2025-03-25 13:36:59 +0530

    feat(socket) : add CORS support and refactor socket server setup

--- src/app.ts
+++ src/app.ts
@@ -1,103 +1,14 @@
-import express, { Express, NextFunction, Request, Response } from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
-interface CustomSocket extends Socket {
-  user?: {
-    user_id?: number;
-    type_id?: number;
-    roomId?: string;
-  };
-}
+import { app, httpServer, PORT } from "./libs/socketSetup";
 
-const app: Express = express();
-const PORT: number = Number(process.env.PORT) || 9000;
+app.use(cors());
+app.use(express.json());
 
-const httpServer: any = createServer(app);
-const io: any = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:5000",
-    methods: ["GET", "POST"]
-  }
+app.post("/create-room", (req: Request, res: Response) => {
+  res.send("Create room and req body is " + JSON.stringify(req.body));
 });
-
-interface SinglePlayerInfo {
-  user_id: number;
-  type_id: number;
-  roomId?: string;
-}
-
-const playerOnline: Record<string, SinglePlayerInfo> = {};
-
-io.use((socket: CustomSocket, next: NextFunction) => {
-  if (socket) {
-    const { userId, typeId } = socket.handshake.query;
-    const user_id: number = Number(userId);
-    const type_id: number = Number(typeId);
-
-    const searchOpponent: SinglePlayerInfo | undefined = Object.values(
-      playerOnline
-    ).find((player: SinglePlayerInfo) => {
-      return player.user_id !== user_id && player.type_id === type_id;
-    });
-    if (searchOpponent && searchOpponent.roomId) {
-      playerOnline[socket?.id] = {
-        user_id,
-        type_id,
-        roomId: searchOpponent.roomId
-      };
-      socket.join(searchOpponent.roomId);
-    } else {
-      playerOnline[socket?.id] = {
-        user_id,
-        type_id
-      };
-      const roomId: string = `${playerOnline[socket.id].user_id}-${playerOnline[socket.id].type_id}`;
-      playerOnline[socket.id].roomId = roomId;
-      socket.join(roomId);
-    }
-
-    next();
-  } else {
-    next(new Error("invalid"));
-  }
-});
-
-io.on("connection", (socket: CustomSocket) => {
-  if (playerOnline[socket.id] && playerOnline[socket.id].roomId) {
-    socket.on(
-      "move",
-      ({
-        from,
-        to,
-        promotion
-      }: {
-        from: string;
-        to: string;
-        promotion: string;
-      }) => {
-        const roomId: any = playerOnline[socket.id]?.roomId;
-        if (!roomId) {
-          return;
-        }
-        socket.to(roomId).emit("move", { from, to, promotion });
-      }
-    );
-  }
-
-  // ✅ Always emit message when a player joins
-  if (playerOnline[socket.id] && playerOnline[socket.id].roomId) {
-    io.to(playerOnline[socket.id].roomId).emit("message", "Hello world");
-  }
-
-  // ✅ Always set up  "move" event for EVERY user
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
-    delete playerOnline[socket.id];
-  });
-});
-
 app.get("/", (req: Request, res: Response) => {
   res.send("sdlkfhlskdfjhsdkljfh");
 });
